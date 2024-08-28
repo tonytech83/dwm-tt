71dda48cf0fa255f86bd81a96b38e03ef2d97270
Anton Petrov <a.petrov.83@gmail.com>
2024-08-28 11:29:54 +0300

    Update display_setup.sh

--- scripts/display_setup.sh
+++ scripts/display_setup.sh
@@ -5,8 +5,8 @@ EXTERNAL_DISPLAY="HDMI-1"
 
 if xrandr | grep "$EXTERNAL_DISPLAY connected"; then
     # If external monitor is connected, use it as the primary display with 75Hz refresh rate
-    xrandr --output $INTERNAL_DISPLAY --off --output $EXTERNAL_DISPLAY --mode 3440x1440 --rate 75 --primary
+    xrandr --output $INTERNAL_DISPLAY --off --output $EXTERNAL_DISPLAY --mode 3440x1440 --dpi 100 --rate 75 --primary
 else
     # If external monitor is not connected, use the internal display
-    xrandr --output $INTERNAL_DISPLAY --auto --primary --output $EXTERNAL_DISPLAY --off
+    xrandr --output $INTERNAL_DISPLAY --auto --primary --output $EXTERNAL_DISPLAY --off --dpi 192
 fi
