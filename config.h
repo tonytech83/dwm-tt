9b80810bb8ab843d3a2dd017e417b4eee4f8de68
tonytech83 <a.petrov.83@gmail.com>
2024-09-07 17:43:28 +0300

    some mods

--- config.h
+++ config.h
@@ -46,6 +46,7 @@ static const char *const autostart[] = {
 	"xset", "s", "noblank", NULL,
 	"xset", "-dpms", NULL,
 	"dbus-update-activation-environment", "--systemd", "--all", NULL,
+	"flameshot", NULL,
 	"picom", "-b", NULL,
 	"nm-applet", NULL,
 	"conky", "-c", "~/.config/conky/dwm/nord.conkyrc", NULL,
@@ -54,7 +55,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "" };
+static const char *tags[] = { "", "", "", "","" };
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
@@ -66,18 +67,18 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      			instance    	title       tags mask   isfloating  monitor */
-	{ "kitty",				NULL,			NULL,		0,			0,			-1 },
-	{ "thorium-browser",    NULL,       	NULL,       1 << 2,     0,          -1 },
-	{ "thunar",             NULL,     		NULL,       0,			1,          -1 },
-	{ NULL,       			"keepassxc",	NULL,       SPTAG(0),   1,   	    -1 },
+	/* class      		instance    	title       	tags mask   	isfloating  	monitor */
+	{ "kitty",		NULL,		NULL,		0,		0,		-1 },
+	{ "thorium-browser",    NULL,       	NULL,       	1 << 2,     	0,		-1 },
+	{ "thunar",             NULL,     	NULL,       	0,		1,      	-1 },
+	{ NULL,       		"keepassxc",	NULL,       	SPTAG(0),   	1,   	    	-1 },
 };
 
 /* layout(s) */
-static const float mfact        = 0.5; /* factor of master area size [0.05..0.95] */
+static const float mfact        = 0.5;  /* factor of master area size [0.05..0.95] */
 static const int nmaster        = 1;    /* number of clients in master area */
 static const int resizehints    = 0;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 static const int attachbelow    = 1;    /* 1 means attach after the currently active window */
  
 
@@ -104,58 +105,57 @@ static const char *launchercmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        					function        argument */
-	{ MODKEY,                       XK_r,      					spawn,          {.v = launchercmd } },
-	{ MODKEY,                       XK_x,      					spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      					spawn,          SHCMD ("thorium-browser") },
-	{ MODKEY,        	            XK_e,               		spawn,          SHCMD ("thunar") },
-	{ MODKEY,                       XK_b,      					togglebar,      {0} },
-	{ MODKEY,                       XK_j,      					focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      					focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      					incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      					incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      					setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      					setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, 					zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    					view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      					killclient,     {0} },
-	{ MODKEY,                       XK_t,      					setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      					setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      					setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  					setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  					togglefloating, {0} },
-	{ MODKEY,                       XK_0,      					view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      					tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  					focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, 					focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  					tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, 					tagmon,         {.i = +1 } },
+	/* modifier                     key        			function        argument */
+	{ MODKEY,                       XK_r,      			spawn,          {.v = launchercmd } },
+	{ MODKEY,                       XK_x,      			spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      			spawn,          SHCMD ("thorium-browser") },
+	{ MODKEY,        	        XK_e,               		spawn,          SHCMD ("thunar") },
+	{ MODKEY,                       XK_b,      			togglebar,      {0} },
+	{ MODKEY,                       XK_j,      			focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      			focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      			incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      			incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      			setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      			setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, 			zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    			view,           {0} },
+	{ MODKEY,                       XK_t,      			setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      			setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      			setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  			setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  			togglefloating, {0} },
+	{ MODKEY,                       XK_0,      			view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      			tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  			focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 			focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  			tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 			tagmon,         {.i = +1 } },
 	/* multimedia */
-	{ 0,                            XF86XK_AudioLowerVolume,   spawn,          SHCMD ("amixer sset Master 5%- unmute")},
-    { 0,                            XF86XK_AudioMute,          spawn,          SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")},
-    { 0,                            XF86XK_AudioRaiseVolume,   spawn,          SHCMD ("amixer sset Master 5%+ unmute")},
+	{ 0,                            XF86XK_AudioLowerVolume,   	spawn,          SHCMD ("amixer sset Master 5%- unmute")},
+    	{ 0,                            XF86XK_AudioMute,          	spawn,          SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")},
+    	{ 0,                            XF86XK_AudioRaiseVolume,   	spawn,          SHCMD ("amixer sset Master 5%+ unmute")},
 
 	/* scratchpads */
-	{ MODKEY,       				XK_y,	   					togglescratch,  {.ui = 0 } },
-	{ MODKEY,       				XK_u,	   					togglescratch,  {.ui = 1 } },
+	{ MODKEY,       		XK_y,	   			togglescratch,  {.ui = 0 } },
+	{ MODKEY,       		XK_u,	   			togglescratch,  {.ui = 1 } },
 
 	/* shiftview*/
-	{ MODKEY,                       XK_Left,   					shiftview,      {.i = -1 } },
-	{ MODKEY,                       XK_Right,  					shiftview,      {.i = +1 } },
+	{ MODKEY,                       XK_Left,   			shiftview,      {.i = -1 } },
+	{ MODKEY,                       XK_Right,  			shiftview,      {.i = +1 } },
 
-	TAGKEYS(                        XK_1,                      					0)
-	TAGKEYS(                        XK_2,                      					1)
-	TAGKEYS(                        XK_3,                      					2)
-	TAGKEYS(                        XK_4,                      					3)
-	TAGKEYS(                        XK_5,                      					4)
-	TAGKEYS(                        XK_6,                      					5)
-	TAGKEYS(                        XK_7,                      					6)
-	TAGKEYS(                        XK_8,                      					7)
-	TAGKEYS(                        XK_9,                      					8)
-	{ MODKEY,                       XK_q,      					killclient,     {0} },
-	{ MODKEY|ControlMask,           XK_q,                       spawn,          SHCMD("powermenu.sh") },
-	{ MODKEY|ShiftMask,             XK_q,      					quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_s,                      	spawn,          SHCMD("systemctl suspend") },
+	TAGKEYS(                        XK_1,                      	0)
+	TAGKEYS(                        XK_2,                      	1)
+	TAGKEYS(                        XK_3,                      	2)
+	TAGKEYS(                        XK_4,                      	3)
+	TAGKEYS(                        XK_5,                      	4)
+	TAGKEYS(                        XK_6,                      	5)
+	TAGKEYS(                        XK_7,                      	6)
+	TAGKEYS(                        XK_8,                      	7)
+	TAGKEYS(                        XK_9,                      	8)
+	{ MODKEY,                       XK_q,      			killclient,     {0} },
+	{ MODKEY|ControlMask,           XK_q,                       	spawn,          SHCMD("powermenu.sh") },
+	{ MODKEY|ShiftMask,             XK_q,      			quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_s,                       	spawn,          SHCMD("systemctl suspend") },
 };
 
 /* button definitions */
@@ -174,4 +174,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
---
+++ scripts/frequency-set.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo cpupower frequency-set --max 2400MHz
